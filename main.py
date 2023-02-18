2dd0a0f2d172d64ebffad1d0832afe15a394410d
Lian <Lian_Hwang@126.com>
2023-02-18 12:17:32 +0800

    基础版本

---
+++ main.py
@@ -0,0 +1,17 @@
+import argparse
+from md_pack.markdown_index import OrderNumber
+
+
+def main():
+    parser = argparse.ArgumentParser(description="markdown typesetting")
+    parser.add_argument('-i', '--inplace', default=0)
+    parser.add_argument('-f', '--filepath', default='')
+    args = parser.parse_args()
+    if args.filepath:
+        OrderNumber().main(args.filepath, args.inplace == '1')
+    else:
+        print('args invalid')
+
+
+if __name__ == '__main__':
+    main()
